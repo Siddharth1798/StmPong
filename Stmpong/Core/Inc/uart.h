ad9bd7c39c482e7f9c3c5ae973c8bee332b296b8
Tharnath <itstharnath@gmail.com>
2024-04-24 14:21:43 -0600

    Revert "Added USART peripheral"
    This reverts commit fcea4efa7ac196d0920ad355cf2928831b26481a.

--- Stmpong/Core/Inc/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UART_H
-#define __UART_H
-
-#include "stm32f072xb.h"
-
-void Error_Handler(void);
-
-#endif /* __MAIN_H */
--- Stmpong/MDK-ARM/RTE/_Stmpong/RTE_Components.h
+++ Stmpong/MDK-ARM/RTE/_Stmpong/RTE_Components.h
@@ -3,12 +3,18 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'StmPong' 
+ * Project: 'Stmpong' 
  * Target:  'Stmpong' 
  */
 
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
 
 
 
